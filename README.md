11a6666d77310485e1dde114552849fa48b57b1c
Wyatt Hudgins <wbhudgins@mail.mccneb.edu>
2022-08-25 20:21:54 -0500

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+Challenge #12
+
+Objectives:
+- Create an Ecommerce Tracker that meets Acceptance Criteria and Technical Acceptance Criteria 
+- Once ready to deply project, push the new code to a repository on GitHub, and provide a link to the demo video.
+- Finally, submit the final work on Campus using a link to your repository and a link to the demo video.
+
+- [Link to Demo Video](https://app.castify.com/view/29e78cd6-a98f-4a3e-bfa2-a1925fc3e285)
---
+++ routes/api/category-routes.js
@@ -0,0 +1,105 @@
+const router = require('express').Router();
+const { Category, Product } = require('../../models');
+
+// The `/api/categories` endpoint
+
+router.get('/', async (req, res) => {
+  // find all categories
+  // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findAll({
+      include: [{ model: Product }],
+    });
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get('/:id', async (req, res) => {
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post('/', async (req, res) => {
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.put('/:id', async (req, res) => {
+  // update a category by its `id` value
+  try {
+    const categoryData = await Category.update(
+      {
+        category_name: req.body.category_name
+      },
+      {
+        where: {
+          id: req.params.id
+        },
+      },
+    );
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  // delete a category by its `id` value
+  try {
+    const categoryData = await Category.destroy({
+      where: {
+        id: req.params.id,
+      },
+    });
+
+    res.status(200).json(categoryData)
+  } catch (err) {
+    res.status(400).json(err)
+  }
+});
+
+module.exports = router;
+
+// router.put('/:book_id', (req, res) => {
+//   Book.update(
+//     {
+//       title: req.body.title,
+//       author: req.body.author,
+//       isbn: req.body.isbn,
+//       pages: req.body.pages,
+//       edition: req.body.edition,
+//       is_paperback: req.body.is_paperback
+//     },
+//     {
+//       where: {
+//         book_id: req.params.book_id,
+//       }
+//     }
+//   )
+//   .then((updatedBook) => {
+//     res.json(updatedBook)
+//   })
+//   .catch((err) => {
+//     console.log(err)
+//     res.json(err)
+//   })
+// });
